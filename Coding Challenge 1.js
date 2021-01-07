627e2d5ddab18827ff67073c3363cf32bdcb8514
Calvin Reed <calvintreed1@gmail.com>
2021-01-07 11:07:15 -0700

    Javascript course beginning.

---
+++ Coding Challenge 1.js
@@ -0,0 +1,9 @@
+const markWeight = 78
+const markHeight = 1.69
+const johnWeight = 92
+const johnHeight = 1.95
+
+let markBMI = markWeight / markHeight ** 2
+let johnBMI = johnWeight / johnHeight ** 2
+
+console.log(markBMI, johnBMI)
