6238541340d72559f3e2d42f8fa1ae26c4da8699
Calvin Reed <calvintreed1@gmail.com>
2021-01-14 10:36:11 -0700

    Coding challenge 3!

--- Coding Challenge 1.js
+++ Coding Challenge 1.js
@@ -1,18 +1,31 @@
-const markWeight = 78
-const markHeight = 1.69
-const johnWeight = 92
-const johnHeight = 1.95
+// Coding Challenge 1 & 2
+// const markWeight = 78
+// const markHeight = 1.69
+// const johnWeight = 92
+// const johnHeight = 1.95
+//
+//
+//
+// let markBMI = markWeight / markHeight ** 2
+// let johnBMI = johnWeight / johnHeight ** 2
+// let markHigherBMI = markBMI > johnBMI;
+//
+// console.log(markBMI, johnBMI, markHigherBMI)
+//
+// if (markBMI > johnBMI) {
+//   console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`)
+// } else {
+//   console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
+// }
 
+// Coding Challenge 3
+let dolphinsScore = (1+1+1/3)
+let koalasScore = (1+1+1/3)
 
-
-let markBMI = markWeight / markHeight ** 2
-let johnBMI = johnWeight / johnHeight ** 2
-let markHigherBMI = markBMI > johnBMI;
-
-console.log(markBMI, johnBMI, markHigherBMI)
-
-if (markBMI > johnBMI) {
-  console.log(`Mark's BMI (${markBMI}) is higher than John's (${johnBMI})!`)
+if (dolphinsScore > koalasScore) {
+  console.log("Dolphins win!")
+} else if (koalasScore > dolphinsScore) {
+  console.log("Koalas win!")
 } else {
-  console.log(`John's BMI (${johnBMI}) is higher than Mark's (${markBMI})!`)
+  console.log("That's a draw!")
 }
